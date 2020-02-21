a78ea93c3be8b310f2764414a120d0715c3761b7
Christopher Ducey <chrisducey01@me.com>
2020-02-20 21:54:53 -0700

    Finished resume section and included link to download resume.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,49 +5,52 @@ const header_offset = $("#header").offset().top;
 const window_height = $(window).height();
 const portfolio_height = $("#portfolio").height();
 
-// scroll functions
-$(window).scroll(function (e) {
-    // add/remove class to navbar when scrolling to hide/show
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 
-    var scroll = $(window).scrollTop();
+    // scroll functions
+    $(window).scroll(function (e) {
+        // add/remove class to navbar when scrolling to hide/show
 
-    if (scroll >= 150 && scroll <= (about_offset - 150)) {
-        $('.navbar').addClass("navbar-hide");
-    } else {
-        $('.navbar').removeClass("navbar-hide");
-    }
+        var scroll = $(window).scrollTop();
 
-    // Make the navbar sticky at the top when it reaches the about me section
-    if(scroll >= (about_offset - 150)){
-        $('.navbar').addClass("sticky");
-    }
-    else{
-        $('.navbar').removeClass("sticky");
-    }
-
-    // Update the active link in the navbar based on the scroll position
-    if(scroll < about_offset){
-        $(".nav-item").find("a").removeClass("active-link");
-        $("#home-link").addClass("active-link");
-    }
-    else if (scroll >= about_offset && scroll <= resume_offset) {
-        $(".nav-item").find("a").removeClass("active-link");
-        $("#about-link").addClass("active-link");
-    }
-    else if (scroll >= resume_offset && scroll <= portfolio_offset) {
-        $(".nav-item").find("a").removeClass("active-link");
-
-        // If portfolio height is shorter than the window height, it'll never hit the top of the window
-        // So set it to the active link when it is at the bottom of the window
-        if( (portfolio_height < window_height) && (scroll >= (resume_offset + (window_height - (portfolio_height + 80)))) ){
-            $("#portfolio-link").addClass("active-link");
-        }else{
-            $("#resume-link").addClass("active-link");            
+        if (scroll >= 150 && scroll <= (about_offset - 150)) {
+            $('.navbar').addClass("navbar-hide");
+        } else {
+            $('.navbar').removeClass("navbar-hide");
         }
-    }
-    else if(scroll >= portfolio_offset){
-        $(".nav-item").find("a").removeClass("active-link");
-        $("#portfolio-link").addClass("active-link");
-    }
 
+        // Make the navbar sticky at the top when it reaches the about me section
+        if (scroll >= (about_offset - 150)) {
+            $('.navbar').addClass("sticky");
+        }
+        else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // Update the active link in the navbar based on the scroll position
+        if (scroll < about_offset) {
+            $(".nav-item").find("a").removeClass("active-link");
+            $("#home-link").addClass("active-link");
+        }
+        else if (scroll >= about_offset && scroll <= resume_offset) {
+            $(".nav-item").find("a").removeClass("active-link");
+            $("#about-link").addClass("active-link");
+        }
+        else if (scroll >= resume_offset && scroll <= portfolio_offset) {
+            $(".nav-item").find("a").removeClass("active-link");
+
+            // If portfolio height is shorter than the window height, it'll never hit the top of the window
+            // So set it to the active link when it is at the bottom of the window
+            if ((portfolio_height < window_height) && (scroll >= (resume_offset + (window_height - (portfolio_height + 80))))) {
+                $("#portfolio-link").addClass("active-link");
+            } else {
+                $("#resume-link").addClass("active-link");
+            }
+        }
+        else if (scroll >= portfolio_offset) {
+            $(".nav-item").find("a").removeClass("active-link");
+            $("#portfolio-link").addClass("active-link");
+        }
+    });
 });
